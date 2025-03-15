82ff1ca9d01984df8b5d107cafd94ab769c7afaa
MalcomQuiros <malcomquiros6@gmail.com>
2025-03-14 23:58:00 -0600

    Creación y modificación de landPages

---
+++ src/app/components/app-layout/app-layout.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './elements/topbar/topbar.component';
+import { SidebarComponent } from './elements/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { LayoutService } from '../../services/layout.service';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { RouterModule } from '@angular/router'; 
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TopbarComponent,
+    SidebarComponent,
+    SvgIconComponent,
+    RouterModule
+  ],
+  templateUrl: './app-layout.component.html',
+})
+export class AppLayoutComponent {
+  public title?: string;
+
+  constructor(public layoutService: LayoutService) {
+    this.layoutService.title.subscribe((title) => (this.title = title));
+  }
+}
